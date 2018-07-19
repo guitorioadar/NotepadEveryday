78e5644b347a219712d1ab20e989a4e2e1c76785
guitorioadar <guitorioadar@gmail.com>
2018-07-19 17:02:42 +0600

    Initial Commit

---
+++ app/src/main/java/com/androvaid/notepadeveryday/notedb/model/Note.java
@@ -0,0 +1,85 @@
+package com.androvaid.notepadeveryday.notedb.model;
+
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int note_id;
+
+    @ColumnInfo(name = "note_content") // column name will be "note_content" instead of "content" in table
+    private String content;
+
+    private String title;
+
+    //private Date date;
+
+
+    /*public Note(String content, String title) {
+        this.content = content;
+        this.title = title;
+        this.date = new Date(System.currentTimeMillis());
+    }*/
+
+    public Note(int note_id, String content, String title) {
+        this.note_id = note_id;
+        this.content = content;
+        this.title = title;
+    }
+
+    public int getNote_id() {
+        return note_id;
+    }
+
+    public void setNote_id(int note_id) {
+        this.note_id = note_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Note)) return false;
+        Note note = (Note) o;
+
+        if (note_id != note.note_id) return false;
+        return title != null ? title.equals(note.title) :  note.title == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = note_id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "note_id=" + note_id +
+                ", content='" + content + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
